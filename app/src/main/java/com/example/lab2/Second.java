5905f7fd70934f27a6a8a29acfb0340aac0e32d5
Alina <netven97@gmail.com>
2020-12-10 21:51:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab2/Second.java
@@ -0,0 +1,10 @@
+package com.example.lab2;
+import android.app.Activity;
+import android.os.Bundle;
+public class Second extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
